f1d835938411690ef959fab5da75c2d024f9df3e
Yar Kakaee <kakaeeyar@gmail.com>
2025-03-28 16:30:29 -0400

    made it fully responsive

--- app/page.js
+++ app/page.js
@@ -19,18 +19,22 @@ const interTight = Inter_Tight({
 export default function Home() {
 	return (
 		<section>
-			<div className="relative min-h-screen bg-[#101014]">
+			<div className="relative min-h-screen bg-[#101014] overflow-hidden">
+				{/* Gradient Background */}
 				<div className="absolute inset-0 pointer-events-none">
-					<div className="absolute w-[1200px] left-1/2 -translate-x-1/2 translate-y-1/6 blur-[125px] opacity-70 transform-gpu">
+					<div className="absolute w-full sm:w-[800px] md:w-[1000px] lg:w-[1200px] max-w-full left-1/2 -translate-x-1/2 translate-y-1/6 blur-[125px] opacity-70 transform-gpu">
 						<Image
 							src="https://cms-assets.unrealengine.com/AiKUh5PQCTaOFnmJDZJBfz/oXIAOr5gQny2cAfPpq02"
 							alt="Abstract light pattern"
 							width={1200}
 							height={1200}
+							className="w-full h-auto"
 							priority
 						/>
 					</div>
 				</div>
+
+				{/* Main Content Layer */}
 				<div className="relative z-10 flex flex-col items-center justify-center min-h-screen text-white space-y-8">
 					<Image
 						src="/assets/OMLogo2.png"
@@ -45,17 +49,19 @@ export default function Home() {
 						OhmMade
 					</h1>
 					<h1
-						className={`text-2xl sm:text-3xl md:text-4xl font-black ${interTight.className}`}
+						className={`text-lg sm:text-3xl md:text-4xl font-black text-center ${interTight.className}`}
 					>
 						From Circuits to Code - All in One Place.
 					</h1>
 					<div className="bg-[#27BBFF] text-[#101014] px-5 py-3 rounded-md text-sm font-medium">
 						Coming Soon
 					</div>
-					<p className="text-sm text-gray-500 mt-2">
+					<p className="text-sm text-gray-500 mt-2 text-center">
 						© {new Date().getFullYear()} OhmMade. All rights
 						reserved.
 					</p>
+
+					{/* Social Media Icons */}
 					<div className="flex space-x-5">
 						<Link
 							href="https://www.youtube.com/@OhmMadeTechs"
@@ -63,7 +69,7 @@ export default function Home() {
 						>
 							<FontAwesomeIcon
 								icon={faYoutube}
-								size="xl"
+								size="lg"
 								style={{ color: '#ffffff' }}
 								className="transition-transform duration-300 ease-in-out hover:scale-110"
 							/>
@@ -71,7 +77,7 @@ export default function Home() {
 						<Link href="https://x.com/OhmMade94739" passHref>
 							<FontAwesomeIcon
 								icon={faXTwitter}
-								size="xl"
+								size="lg"
 								style={{ color: '#ffffff' }}
 								className="transition-transform duration-300 ease-in-out hover:scale-110"
 							/>
@@ -79,7 +85,7 @@ export default function Home() {
 						<Link href="https://github.com/OhmMadeTech" passHref>
 							<FontAwesomeIcon
 								icon={faGithub}
-								size="xl"
+								size="lg"
 								style={{ color: '#ffffff' }}
 								className="transition-transform duration-300 ease-in-out hover:scale-110"
 							/>
@@ -90,7 +96,7 @@ export default function Home() {
 						>
 							<FontAwesomeIcon
 								icon={faLinkedin}
-								size="xl"
+								size="lg"
 								style={{ color: '#ffffff' }}
 								className="transition-transform duration-300 ease-in-out hover:scale-110"
 							/>
@@ -101,7 +107,7 @@ export default function Home() {
 						>
 							<FontAwesomeIcon
 								icon={faInstagram}
-								size="xl"
+								size="lg"
 								style={{ color: '#ffffff' }}
 								className="transition-transform duration-300 ease-in-out hover:scale-110"
 							/>
@@ -112,7 +118,7 @@ export default function Home() {
 						>
 							<FontAwesomeIcon
 								icon={faTiktok}
-								size="xl"
+								size="lg"
 								style={{ color: '#ffffff' }}
 								className="transition-transform duration-300 ease-in-out hover:scale-110"
 							/>
