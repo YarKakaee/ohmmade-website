6c833f3fe32215139f328d03abdf39c7fbafe630
Yar Kakaee <kakaeeyar@gmail.com>
2025-05-08 04:19:48 -0400

    feat: redesigned Coming Soon page

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ const inter = Inter({
 });
 
 export const metadata = {
-	title: 'OhmMade | Electronics Made Simple. Projects Worth Sharing.',
+	title: 'OhmMade | Launching Q3 2025',
 	description:
 		'OhmMade is where makers publish, discover, and share electronics projects with the world. From Raspberry Pi builds to Arduino tutorials, OhmMade gives you the tools to showcase your work, inspire others, and explore step-by-step guides — all in one beginner-friendly, beautifully designed platform.',
 	keywords: [
@@ -36,7 +36,7 @@ export const metadata = {
 		siteName: 'OhmMade',
 		images: [
 			{
-				url: '/assets/og-image.png',
+				url: '/og-image.png',
 				width: 1200,
 				height: 630,
 				alt: 'OhmMade Image',
@@ -49,7 +49,7 @@ export const metadata = {
 		title: 'OhmMade | Electronics Made Simple. Projects Worth Sharing.',
 		description:
 			'Discover, build, and share your electronics projects with OhmMade.',
-		images: ['/assets/og-image.png'],
+		images: ['/og-image.png'],
 	},
 };
 
