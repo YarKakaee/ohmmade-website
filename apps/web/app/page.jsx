72114e61e2f89d882caec78c4ec10adb89eb7869
Yar Kakaee <kakaeeyar@gmail.com>
2025-07-30 04:45:25 -0400

    refactor: enforce dynamic rendering in Home component to prevent prerendering

--- apps/web/app/page.jsx
+++ apps/web/app/page.jsx
@@ -4,6 +4,8 @@ import ProjectsSection from './components/sections/ProjectsSection';
 import Scrollytelling from './components/sections/Scrollytelling';
 import './globals.css';
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
 	return (
 		<section>
