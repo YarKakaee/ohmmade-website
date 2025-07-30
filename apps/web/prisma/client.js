25826c61025fd1d5c68690a8f51994ef097bd7ac
Yar Kakaee <kakaeeyar@gmail.com>
2025-07-30 03:30:51 -0400

    refactor: optimize Prisma client initialization for production and development environments

--- apps/web/prisma/client.js
+++ apps/web/prisma/client.js
@@ -1,11 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
-const prismaClientSingleton = () => {
-	return new PrismaClient();
-};
+let prisma;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+if (process.env.NODE_ENV === 'production') {
+	prisma = new PrismaClient();
+} else {
+	if (!globalThis.prismaGlobal) {
+		globalThis.prismaGlobal = new PrismaClient();
+	}
+	prisma = globalThis.prismaGlobal;
+}
 
 export default prisma;
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
